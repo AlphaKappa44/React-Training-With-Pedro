63b9baacc64146c832e7d87dc9f3071bb24969fe
Alexandre Kelifa <alexandre.kelifa@hotmail.com>
2022-07-31 16:11:25 +0200

    Ternary operator vs if ilse

--- src/App.js
+++ src/App.js
@@ -5,17 +5,25 @@ function App() {
     <div className="App">
       <div><h1>Hello </h1>
         <div>
-        <User name="Jean-Patrick" email="jp.admin@example.com" password="admin" />
+        <User name="Jean-Patrick" age="17" email="jp.admin@example.com" password="admin" />
         <Job title="Electrician" description="Create a new blog post"  progress="0" />
         </div>
         <div>
-        <User name="René" email="rené@example.com" password="admin" />  
+        <User name="René" age="45" email="rené@example.com" password="admin" />  
         <Job title="Farmer" description="Infinity of posts" progress="100" />   
         </div> 
       </div>
     </div>
   );
 }
+
+const age = 45;
+// if (age >= 18) {
+//   console.log('Is over age of 18 years');
+// } else if (age >= 19) {
+//   console.log('Is UNDER age of 18 years!');
+// } Same as:
+age >= 18 ? console.log('Is over age of 18 years') : console.log('Is UNDER age of 18 years!');
 
 const User = (props) => {
 
@@ -24,6 +32,7 @@ const User = (props) => {
       <h2>{props.name}</h2>
       <p>{props.email} </p>
       <p>{props.password} </p>
+      <p>{props.age} </p>
     </div>
   );
 }
