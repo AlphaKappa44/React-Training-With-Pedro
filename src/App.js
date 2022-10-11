ae063f3c254ebe85b094dd75627ec6c10cd91705
Alexandre Kelifa <alexandre.kelifa@hotmail.com>
2022-10-11 16:07:52 +0200

    ternary operator on CSS && conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,51 +1,42 @@
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <div><h1>Hello </h1>
-        <div>
-        <User name="Jean-Patrick" age="17" email="jp.admin@example.com" password="admin" />
-        <Job title="Electrician" description="Create a new blog post"  progress="0" />
-        </div>
-        <div>
-        <User name="René" age="45" email="rené@example.com" password="admin" />  
-        <Job title="Farmer" description="Infinity of posts" progress="100" />   
-        </div> 
-      </div>
+  // Change the age and see:
+  const age = 19;
+  // const age = 17;
+  let isGreen = true;
+// IF ELSE statements:
+  //     if (age >= 18) {
+  // console.log('Is over age of 18 years')
+  // return <h1>Is over age of 18 years</h1>
+  //   } else if (age <= 19) {
+  // console.log('Is UNDER age of 18 years!')
+  // return <h1>Is UNDER age of 18 years</h1>
+  return ( 
+    <div className='App'>
+      {/* If statement on logic */}
+      {age >= 18 ? <p>Is OVER age of 18 years </p> : <p>Is UNDER age of 18 years</p> }
+      {age >= 18 ? isGreen=true : isGreen = false}
+
+      {/* Ternary operator on CSS => if true then green else red */}
+      <h1 style={{color: isGreen? "green" : "red"}}>COLOR</h1>
+
+      {/* If True, render a tag (button) */}
+      {isGreen 
+      &&  <button> Color isGreen!</button>}
+          {isGreen 
+      && <p>You are aloud to click on the button</p> 
+      }
+
     </div>
   );
 }
 
-const age = 45;
-// if (age >= 18) {
-//   console.log('Is over age of 18 years');
-// } else if (age >= 19) {
-//   console.log('Is UNDER age of 18 years!');
-// } Same as:
-age >= 18 ? console.log('Is over age of 18 years') : console.log('Is UNDER age of 18 years!');
 
-const User = (props) => {
 
-  return (
-    <div className="App">
-      <h2>{props.name}</h2>
-      <p>{props.email} </p>
-      <p>{props.password} </p>
-      <p>{props.age} </p>
-    </div>
-  );
-}
 
-const Job = (props) => {
 
-  return (
-    <div className="App">
-      <h2>{props.title}</h2>
-      <p>{props.description} </p>
-      <p>{props.progress} </p>
-    </div>
-  );
-}
+
+
 
 export default App;
